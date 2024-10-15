af3b230dc6c3ce8b40707b7afb9bcfe9a70955f7
Divyam <47589864+divyam234@users.noreply.github.com>
2024-10-15 14:41:03 +0530

    Update start.sh

--- postgres/start.sh
+++ postgres/start.sh
@@ -19,10 +19,16 @@ echo "Stopping temporary PostgreSQL process..."
 kill -s TERM $PID
 wait $PID
 
-# Modify the PostgreSQL configuration
-echo "Modifying PostgreSQL configuration..."
-echo "shared_preload_libraries = 'pg_cron'" >> /var/lib/postgresql/data/postgresql.conf
-echo "cron.database_name = '${POSTGRES_DB:-postgres}'" >> /var/lib/postgresql/data/postgresql.conf
+# Modify the PostgreSQL configuration if not already done
+CONFIG_FILE="/var/lib/postgresql/data/postgresql.conf"
+if ! grep -q "shared_preload_libraries = 'pg_cron'" "$CONFIG_FILE"; then
+    echo "Modifying PostgreSQL configuration..."
+    echo "shared_preload_libraries = 'pg_cron'" >> "$CONFIG_FILE"
+fi
+
+if ! grep -q "cron.database_name = '${POSTGRES_DB:-postgres}'" "$CONFIG_FILE"; then
+    echo "cron.database_name = '${POSTGRES_DB:-postgres}'" >> "$CONFIG_FILE"
+fi
 
 echo "Starting PostgreSQL..."
 # Has to run as the postgres user
